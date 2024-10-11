a2e26164b312e7f52c947490c5e566b96d0df4f8
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-11 14:03:48 +0200

    Created db

--- index.php
+++ index.php
@@ -1,77 +1,10 @@
 <?php
 include_once __DIR__ . "/models/Category.php";
 include_once __DIR__ . "/models/Product.php";
+include_once __DIR__ . "/models/db.php";
 
 $cani = new Category("Cani");
 $gatti = new Category("Gatti");
-
-
-$products = [
-    new Product(
-        "Palla morbida",
-        3.99,
-        "https://cdn.icon-icons.com/icons2/2242/PNG/512/perro_icon_134885.png",
-        "https://www.lelepetshop.it/images/thumbs/0002949_gioco-cane-palla-tennis-gigante-maxi-15-cm-diametro_550.png",
-        "Giochi per cani",
-        $cani
-    ),
-    new Product(
-        "Cuccia per gatto",
-        29.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://croci.net/cdn/shop/files/Cuccia_morbida_per_gatto_Bobby_Poilu.jpg?v=1709817054&width=850",
-        "Accessori per gatti",
-        $gatti
-    ),
-    new Product(
-        "Crocchette per cani",
-        9.99,
-        "https://cdn.icon-icons.com/icons2/2242/PNG/512/perro_icon_134885.png",
-        "https://www.pacopetshop.it/16048-home_default/naxos-wellness-valle-bruna-mini-adult-per-cani.jpg",
-        "Cibo per cani",
-        $cani
-    ),
-    new Product(
-        "Gioco per gatti",
-        11.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://www.dmail.it/on/demandware.static/-/Sites-dret-catalog/default/dw719c0a68/images_dmail/large/509459l_1.jpg",
-        "Giochi per gatti",
-        $gatti
-    ),
-    new Product(
-        "Cibo umido con fegato",
-        1.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://www.naturepetshop.it/wp-content/uploads/leonardo-fegato-400-gr-lattine.jpg",
-        "Cibo per gatti",
-        $gatti
-    ),
-    new Product(
-        "Salmone e ribes",
-        3.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://www.naturepetshop.it/wp-content/uploads/UMIDO_ADULT_SALMONE-RIBES_400g-300x300-1.jpg",
-        "Cibo per cani",
-        $cani
-    ),
-    new Product(
-        "Cuccia per cani",
-        22.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://www.gardenbedettishop.com/16599-large_default/cuccia-per-cani-charles-50-ferplast.jpg",
-        "Accessori per cani",
-        $cani
-    ),
-    new Product(
-        "Peluche per cani",
-        4.99,
-        "https://images.freeimages.com/vhq/images/previews/bdc/cat-icons-4-43748.png",
-        "https://www.sleepypets.it/944-medium_default/gioco-per-cani-personalizzato.jpg",
-        "Giochi per cani",
-        $cani
-    ),
-];
 
 ?>
 
--- models/Product.php
+++ models/Product.php
@@ -4,11 +4,11 @@ include_once __DIR__ . "/Category.php";
 
 class Product
 {
-    public $title;
-    public $price;
-    public $icon;
-    public $image;
-    public $type;
+    public string $title;
+    public float $price;
+    public string $icon;
+    public string $image;
+    public string $type;
     public $category;
 
     public function __construct(string $title, float $price, string $icon, string $image, string $type, Category $category,)
@@ -20,28 +20,64 @@ class Product
         $this->type = $type;
         $this->category = $category;
     }
-    public function getTitle()
+
+    public function getTitle(): string
     {
         return $this->title;
     }
 
-    public function getPrice()
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getPrice(): float
     {
         return $this->price;
     }
 
-    public function getImage()
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getIcon(): string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(string $icon): void
+    {
+        $this->icon = $icon;
+    }
+
+    public function getImage(): string
     {
         return $this->image;
     }
 
-    public function getType()
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function getType(): string
     {
         return $this->type;
     }
 
-    public function getCategory()
+    public function setType(string $type): void
     {
-        return $this->category->getName();
+        $this->type = $type;
+    }
+
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(Category $category): void
+    {
+        $this->category = $category;
     }
 }
